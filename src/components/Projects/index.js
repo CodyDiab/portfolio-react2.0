c2011bae78a69a8e8af990fafc3aeda37b40925b
Diab <emailcodydiab@gmail.com>
2020-08-01 13:07:57 -0500

    edits

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,44 +5,80 @@ import { Carousel } from 'react-responsive-carousel';
 
 function Projects() {
     return(
-        <Carousel showThumbs={false}>
-        <a href="https://stormy-scrubland-17368.herokuapp.com/" tagtet="_blank" rel="noopener noreferrer">
+        <Carousel showThumbs={false} >
+        
             <div className="item ">
                 <img src={require("../../assets/scatterbrain.png")} alt="screenshot"></img>
-                <p>Scatterbrain</p>
+                <a href="https://stormy-scrubland-17368.herokuapp.com/" tagtet="_blank" rel="noopener noreferrer">
+                Visit Scatterbrain
+                </a>
+                <br></br>
+                <a href="https://github.com/CodyDiab/ScatterBrain" tagtet="_blank" rel="noopener noreferrer">
+                    GitHub Repo
+                    </a>
             </div>
-         </a>
-        <a href="https://codydiab.github.io/dm-emergency-paper-locator/" target="_blank" rel="noopener noreferrer">
+         
+       
             <div className="item" >
         <img  src={require("../../assets/Screenshot (11).png")} alt="screenshot"></img>
-           <p>Paper Locator</p>
+        <a href="https://codydiab.github.io/dm-emergency-paper-locator/" target="_blank" rel="noopener noreferrer">
+           Visit Paper Locator
+           </a>
+           <br></br>
+           <a href="https://github.com/CodyDiab/ScatterBrain" tagtet="_blank" rel="noopener noreferrer">
+               GitHub Repo
+           </a>
         </div>
-         </a>
         
-        <a href="https://codydiab.github.io/IRIS-wapp/" tagtet="_blank" rel="noopener noreferrer">
+        
+        
         <div className="item">
             <img src={require("../../assets/IRISscreenshot.png")} alt="screenshot"></img>
-             <p>IRIS Weather</p>
+            <a href="https://codydiab.github.io/IRIS-wapp/" tagtet="_blank" rel="noopener noreferrer">
+             IRIS Weather
+             </a>
+             <br></br>
+             <a href="https://github.com/CodyDiab/ScatterBrain" tagtet="_blank" rel="noopener noreferrer">
+                 GitHub Repo
+             </a>
         </div>
-        </a>
-        <a href="https://codydiab.github.io/git-it-done/" tagtet="_blank" rel="noopener noreferrer">
+       
+       
         <div className="item ">
             <img src={require("../../assets/git-it-done.png")} alt="screenshot"></img>
-            <p>Git-It-Done</p>
+            <a href="https://codydiab.github.io/git-it-done/" tagtet="_blank" rel="noopener noreferrer">
+            Git-It-Done
+            </a>
+            <br></br>
+            <a href="https://github.com/CodyDiab/ScatterBrain" tagtet="_blank" rel="noopener noreferrer">
+                GitHub Repo
+            </a>
         </div>
-        </a>
-        <a href="https://codydiab.github.io/Accessibility-Boost-Project/" target="_blank" rel="noopener noreferrer">
+       
+       
             <div className="item">
            <img src={require("../../assets/acess-boost.png")} alt="screenshot"></img>
-           <p>Horiseon</p>
+           <a href="https://codydiab.github.io/Accessibility-Boost-Project/" target="_blank" rel="noopener noreferrer">
+           Horiseon
+           </a>
+           <br></br>
+           <a href="https://github.com/CodyDiab/ScatterBrain" tagtet="_blank" rel="noopener noreferrer">
+               GitHub Repo
+           </a>
           </div>
-       </a>
-        <a href="https://codydiab.github.io/Work-Day-Scheduler/" tagtet="_blank" rel="noopener noreferrer">
+      
+       
         <div className="item">
             <img src={require("../../assets/work-day-sched..png")} alt="screenshot"></img>
-            <p>Work-Day-Scheduler</p>
+            <a href="https://codydiab.github.io/Work-Day-Scheduler/" tagtet="_blank" rel="noopener noreferrer">
+            Work-Day-Scheduler
+            </a>
+            <br></br>
+            <a href="https://github.com/CodyDiab/ScatterBrain" tagtet="_blank" rel="noopener noreferrer">
+                GitHub Repo
+            </a>
         </div>
-        </a>
+       
         
     </Carousel>
     )
