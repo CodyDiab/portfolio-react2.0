2bda13b7f063142aca8e7c72cb0d160f3d683552
Diab <emailcodydiab@gmail.com>
2020-08-01 13:30:26 -0500

    Updates

---
+++ precache-manifest.68c172b60a45d4e919abc7afb4c1421a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c12f7e88d3a1b4d6a6376a1d1023b0",
+    "url": "/portfolio-react2.0/index.html"
+  },
+  {
+    "revision": "b58f9573c2a8a45d13f2",
+    "url": "/portfolio-react2.0/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "0e6993277b72a7081d39",
+    "url": "/portfolio-react2.0/static/css/main.8f0c0705.chunk.css"
+  },
+  {
+    "revision": "b58f9573c2a8a45d13f2",
+    "url": "/portfolio-react2.0/static/js/2.3ac914f8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio-react2.0/static/js/2.3ac914f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e6993277b72a7081d39",
+    "url": "/portfolio-react2.0/static/js/main.87549048.chunk.js"
+  },
+  {
+    "revision": "1139c145c80d1734f0ae",
+    "url": "/portfolio-react2.0/static/js/runtime-main.a2cd90fe.js"
+  },
+  {
+    "revision": "d1c91f337a7ae97673d2ab344dde4222",
+    "url": "/portfolio-react2.0/static/media/IRISscreenshot.d1c91f33.png"
+  },
+  {
+    "revision": "6fad5baba2defcc08322f1e2821a5c51",
+    "url": "/portfolio-react2.0/static/media/Screenshot (11).6fad5bab.png"
+  },
+  {
+    "revision": "c50b97a00e3128571f663cb606e9f7e2",
+    "url": "/portfolio-react2.0/static/media/acess-boost.c50b97a0.png"
+  },
+  {
+    "revision": "fc7b6398aef766c3230b9be599528b38",
+    "url": "/portfolio-react2.0/static/media/background-joshua.fc7b6398.jpg"
+  },
+  {
+    "revision": "cffa46737e25011ea2ce6ae86af087f0",
+    "url": "/portfolio-react2.0/static/media/git-it-done.cffa4673.png"
+  },
+  {
+    "revision": "88d2abd94df49658074f57862acb7610",
+    "url": "/portfolio-react2.0/static/media/logo-2.88d2abd9.svg"
+  },
+  {
+    "revision": "ffa92d08c6ebe3666b7219853d2a94ab",
+    "url": "/portfolio-react2.0/static/media/profile-image.ffa92d08.jpg"
+  },
+  {
+    "revision": "882d5bcb404fde091e7bcd9bceb5309f",
+    "url": "/portfolio-react2.0/static/media/scatterbrain.882d5bcb.png"
+  },
+  {
+    "revision": "a7f6d084048a64e7ef5700ad0ac1193f",
+    "url": "/portfolio-react2.0/static/media/work-day-sched..a7f6d084.png"
+  }
+]);
